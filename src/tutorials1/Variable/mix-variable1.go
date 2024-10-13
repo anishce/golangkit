36ea998cb62faa453531130bb67e96b8e2f1b005
Anish SE <anish.cse7@gmail.com>
2024-10-13 12:41:28 +0530

    Update mix-variable1.go

--- src/tutorials1/Variable/mix-variable1.go
+++ src/tutorials1/Variable/mix-variable1.go
@@ -5,11 +5,14 @@ import "fmt"
 func main() {
 	var x float64 = 40.0
 
+vat z string ="Hello World"
 	y := 20
 
 	fmt.Println(x)
 	fmt.Println(y)
-
+fmt.Println(z)
 	fmt.Printf("x is type of %T\n", x)
 	fmt.Printf("y is type of %T\n", y)
+fmt.Printf("z is type of %T\n", z)
+
 }
